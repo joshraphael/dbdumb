ff3c6ccac2a94c24731bb29ac14097f8755ad241
Joshua Raphael <josh.n.raphael@gmail.com>
2019-11-04 13:44:12 -0700

    add url argument to cli

--- dbdumb/dbdumb.py
+++ dbdumb/dbdumb.py
@@ -1,14 +1,22 @@
 import sqlalchemy
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, exc
 from dbdumb.click import types
 import click
+import sys
 
 @click.command()
 @click.option('-f', '--format', 'format', type=types.Format(), required=True, help='Output format.')
-def dbdumb(format):
+@click.argument('url', type=click.STRING)
+def dbdumb(format, url):
     print('in dbdumb')
-    #engine = create_engine("sqlite:////home/user/projects/dbdumb/dbdumb.db")
-    engine = create_engine("postgresql://postgres:pgpassword@localhost:5432/dbdumb")
+    try:
+        #engine = create_engine("sqlite:////home/user/projects/dbdumb/dbdumb.db", pool_pre_ping=True)
+        #engine = create_engine("postgresql://postgres:pgpassword@localhost:5432/dbdumb", pool_pre_ping=True)
+        engine = create_engine(url, pool_pre_ping=True)
+        engine.connect()
+    except exc.SQLAlchemyError as e:
+        print("ERROR:",e)
+        return
     print(sqlalchemy.__version__)
     print(engine.table_names())
     meta = MetaData(None)
